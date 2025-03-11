5f515c0105e812c0fc490f889e07a0655c931346
Raihan Ahmed <raiihan2500@gmail.com>
2025-03-11 12:36:08 +0600

    Extract methods for readability

--- taskList.java
+++ taskList.java
@@ -8,23 +8,35 @@ class TaskList {
   static final int REMOVE = 2;
   static final int SHOW = 3;
   static final int EXIT = 4;
+
+  static void add() {
+    System.out.print("N: ");
+    scanner.nextLine();
+    list.add(scanner.nextLine());  }
+
+  static void remove() {
+    System.out.print("I: ");
+        int i = scanner.nextInt();
+        if (i >= 0 && i < list.size())
+          list.remove(i);
+  }
+
+  static void show() {
+    for (int i = 0; i < list.size(); i++) {
+      System.out.println(i + ": " + list.get(i));
+    }
+  }
+
   public static void main(String[] a) {
     while (true) {
       System.out.println("1: Add 2: Remove 3: Show 4: Exit");
       int choice = scanner.nextInt();
       if (choice == ADD) {
-        System.out.print("N: ");
-        scanner.nextLine();
-        list.add(scanner.nextLine());
+        add();        
       } else if (choice == REMOVE) {
-        System.out.print("I: ");
-        int i = scanner.nextInt();
-        if (i >= 0 && i < list.size())
-          list.remove(i);
+        remove();
       } else if (choice == SHOW) {
-        for (int j = 0; j < list.size(); j++) {
-          System.out.println(j + ": " + list.get(j));
-        }
+        show();
       } else if (choice == EXIT)
         break;
     }
